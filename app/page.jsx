7c7aa8aafd2071bd2873a82c82c8efafaeb29ffd
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2025-03-18 10:31:02 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -145,6 +145,11 @@ export default function ProfilePage() {
               <h1 className="text-4xl font-bold text-gray-800 mb-2">{profile.name}</h1>
               <h2 className="text-2xl font-semibold text-blue-600 mb-6">Senior Forex Trader,<span className="text-red-600">Trainer </span> and Building Expert Advisor</h2>
               <p className="text-lg leading-relaxed text-gray-700 mb-8">{profile.bio}</p>
+                <div>
+                <h3 className="text-xl font-medium text-gray-800 mb-3">Contact</h3>
+                <p className="text-gray-600 my-2">Email: {profile.contact.email}</p>
+                <p className="text-gray-600 my-2">Phone: {profile.contact.Phone}</p>
+              </div>
               
               <div className="mb-8">
                 <h3 className="text-xl font-medium text-gray-800 mb-3">Expertise</h3>
@@ -166,11 +171,7 @@ export default function ProfilePage() {
                 </p>
               </div>
               
-              <div>
-                <h3 className="text-xl font-medium text-gray-800 mb-3">Contact</h3>
-                <p className="text-gray-600 my-2">Email: {profile.contact.email}</p>
-                <p className="text-gray-600 my-2">Phone: {profile.contact.Phone}</p>
-              </div>
+            
             </div>
           </section>
 
